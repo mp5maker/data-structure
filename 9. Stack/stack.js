482a0e427ded20e9fa90a16e5b6fa05d580a2787
mp5maker <khan.photon@gmail.com>
2022-07-23 03:13:31 +0600

    :sparkles: added preliminary stack

--- 9. Stack/stack.js
+++ 9. Stack/stack.js
@@ -1,28 +1,27 @@
-class Node {
-  constructor(value) {
-    this.value = value
-    this.next = null
-  }
-}
-
+// Last In First Out
 class Stack {
   constructor() {
     this.arr = [];
   }
 
-  push(value) {
-    this.arr.push(value);
+  pop() {
+    return this.arr.pop();
   }
 
-  unshift() {
-    this.arr.shift();
+  push(val) {
+    this.arr.push(val);
+  }
+
+  peek() {
+    return this.arr[this.arr.length - 1];
   }
 }
 
-const s = new Stack()
-s.push(1) // 1
-s.push(5) // 1, 5
-s.push(7) // 1, 5, 7
-s.push(8) // 1, 5, 7, 8
-s.unshift() // 5, 7, 8
-console.log(s.arr)
+const s = new Stack();
+s.push(1); // 1
+s.push(5); // 1, 5
+s.push(7); // 1, 5, 7
+s.push(8); // 1, 5, 7, 8
+s.pop(); // 1, 5, 7
+console.log(s.peek()); // 7
+console.log(s.arr);
