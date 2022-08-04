fb7f90c2e2eec6016b7ca4e5cb74e50b63d096e8
mp5maker <khan.photon@gmail.com>
2022-08-04 17:37:30 +0600

    :hammer_and_wrench: fix: updated complexity of twoSum from O(n)

--- 27. Exercises/twoSum.js
+++ 27. Exercises/twoSum.js
@@ -1,19 +1,14 @@
-const twoSum = (nums, target) => {
-  const helper = (arr, reach = 0, startIndex = 0, indexes = []) => {
-    if (reach === target && indexes.length == 2) return indexes;
-
-    for (let i = startIndex; i < arr.length; i++) {
-      const current = arr[i];
-      const newValue = reach + current;
-      const result = helper(arr, newValue, i + 1, [...indexes, i]);
-      if (result !== null) return result;
+const twoSum = function (nums, target) {
+  const dict = {};
+  for (let i = 0; i < nums.length; i++) {
+    const current = nums[i];
+    if (current in dict) {
+      return [dict[current], i];
+    } else {
+      dict[target - current] = i;
     }
-
-    return null
-  };
-
-  const finalResult =  helper(nums);
-  return finalResult ? finalResult : null
+  }
+  return [];
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
@@ -21,18 +16,3 @@ console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
 console.log(twoSum([0, 4, 3, 0], 0));
 console.log(twoSum([-1, -2, -3, -4, -5], -8));
-
-// 3 2 4
-
-// 3
-// [0]
-// 2      4
-// [0,1] [0.2]
-// null   null
-
-// 2
-// [1]
-// 4
-// [1, 2]
-
-// 2
